20773119beb7416188f6f267b0102ee14895eabc
Daniel Schneider <daschne8@gmail.com>
2019-06-12 21:52:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,6 +6,6 @@ function driverNamesWithRevenueOver(drivers,revenue) {
 }
 function exactMatch(drivers,ob){
   key = Object.keys(ob)[0]
-  value = Object.values[0]
+  value = Object.values(ob)[0]
   return drivers.map(function (driver) {return driver[key] === value})
 }
